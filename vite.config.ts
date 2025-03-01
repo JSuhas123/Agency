e5cd576980a6edbb6a08945c46c9f246ad342247
JSuhas123 <151022324+JSuhas123@users.noreply.github.com>
2025-03-01 14:06:23 +0530

    Agency

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-import react from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
   build: {
-    rollupOptions: {
-      external: ["framer-motion", "react-intersection-observer","react-router-dom"],
-    },
+    outDir: 'dist',
+    emptyOutDir: true,
   },
 });
