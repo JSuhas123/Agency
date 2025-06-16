8272946c5ef7d7d6fd9493112a908fe0649efb09
JSuhas123 <151022324+JSuhas123@users.noreply.github.com>
2025-06-16 22:37:14 +0530

    fend bend connection

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,15 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000', // Point to backend
+        changeOrigin: true,
+        secure: false
+      }
+    }
+  },
   build: {
     outDir: 'dist',
     emptyOutDir: true,
